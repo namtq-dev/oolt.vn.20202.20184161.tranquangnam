3e0478a43f379001fa2d080c9ebe9dbc23beebec
namtq184161 <quangnam260800@gmail.com>
2021-03-09 17:52:08 +0700

    Aims

---
+++ AimsProject_lab03/src/main/java/DigitalVideoDisc.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Wind
+ */
+public class DigitalVideoDisc {
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+    }
+
+    public DigitalVideoDisc(int length) {
+        this.length = length;
+    }
+
+    public DigitalVideoDisc(float cost) {
+        this.cost = cost;
+    }
+    
+    
+}
---
+++ AimsProject_lab03/src/main/java/Order.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Wind
+ */
+public class Order {
+    public static final int MAX_NUMBERS_ORDERED = 10;
+    private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    int qtyOrdered;
+    
+    
+}
